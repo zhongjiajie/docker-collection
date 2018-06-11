8841af6bf771dea12472135aba5e22db2a637f24
zhongjiajie <zhongjiajie955@hotmail.com>
2018-06-11 17:24:22 +0800

    fix JAVA_OPT not work bug

--- script/entrypoint.sh
+++ script/entrypoint.sh
@@ -7,13 +7,13 @@ case "$1" in
   namesrv)
     # rocketmq-4.1.0-incubating `runnamesrv.sh` original memory setting is `JAVA_OPT="${JAVA_OPT} -server -Xms4g -Xmx4g -Xmn2g -XX:PermSize=128m -XX:MaxPermSize=320m`"
     : "${JVM_MEMORY:="-Xms1g -Xmx1g -Xmn512m -XX:PermSize=64m -XX:MaxPermSize=256m"}"
-    sed -i "s/-server -Xms4g -Xmx4g -Xmn2g -XX:PermSize=128m -XX:MaxPermSize=320m/${JVM_MEMORY}/g" runserver.sh
+    sed -i "s/-server -Xms4g -Xmx4g -Xmn2g -XX:PermSize=128m -XX:MaxPermSize=320m/-server ${JVM_MEMORY}/g" runserver.sh
     exec sh mqnamesrv
     ;;
   broker)
     # rocketmq-4.1.0-incubating `runbroker.sh` original memory setting is `JAVA_OPT="${JAVA_OPT} -server -Xms8g -Xmx8g -Xmn4g`"
     : "${JVM_MEMORY:="-Xms2g -Xmx2g -Xmn1g"}"
-    sed -i "s/-server -Xms8g -Xmx8g -Xmn4g/${JVM_MEMORY}/g" runbroker.sh
+    sed -i "s/-server -Xms8g -Xmx8g -Xmn4g/-server ${JVM_MEMORY}/g" runbroker.sh
     exec sh mqbroker -n "$2":9876
     ;;
   *)
